6cda51337ff7e447957145dfb809aa5607586e27
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-10-21 21:43:39 +0800

    Update header.php

--- header.php
+++ header.php
@@ -2,10 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <!-- <title><?php the_title(); ?></title> -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <!-- <link rel="icon" href="path/to/favicon.ico" type="image/x-icon"> -->
-        <!-- <meta name="description" content="A description of your page here."> -->
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
