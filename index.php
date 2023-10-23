7a585e12dfc6d95caf7004dbac0cfc191fd5d38a
djmalibiran <33365949+djmalibiran@users.noreply.github.com>
2023-10-23 22:40:05 +0800

    Add Hero Buttons

--- index.php
+++ index.php
@@ -14,8 +14,24 @@
                 echo '<p>' . esc_html(get_field('hero_text')) . '</p>';
             }
             ?>
-            <a href="#projects" role="button" class="btn btn-primary btn-lg me-3">Projects</a>
-            <a href="#donate" role="button" class="<?php echo ($hero_image) ? 'contrast' : 'secondary'; ?>">Donate</a>
+            <?php 
+            $hero_button_1 = get_field('hero_button_1');
+            if( $hero_button_1 ): 
+                $hero_button_1_url = $hero_button_1['url'];
+                $hero_button_1_title = $hero_button_1['title'];
+                $hero_button_1_target = $hero_button_1['target'] ? $hero_button_1['target'] : '_self';
+                ?>
+                <a role="button" href="<?php echo esc_url( $hero_button_1_url ); ?>" target="<?php echo esc_attr( $hero_button_1_target ); ?>"><?php echo esc_html( $hero_button_1_title ); ?></a>
+            <?php endif; ?>
+            <?php 
+            $hero_button_2 = get_field('hero_button_2');
+            if( $hero_button_2 ): 
+                $hero_button_2_url = $hero_button_2['url'];
+                $hero_button_2_title = $hero_button_2['title'];
+                $hero_button_2_target = $hero_button_2['target'] ? $hero_button_2['target'] : '_self';
+                ?>
+                <a role="button" class="<?php echo ($hero_image) ? 'contrast outline' : 'secondary outline'; ?>" href="<?php echo esc_url( $hero_button_2_url ); ?>" target="<?php echo esc_attr( $hero_button_2_target ); ?>"><?php echo esc_html( $hero_button_2_title ); ?></a>
+            <?php endif; ?>
         </div>
     </section>
     <?php if( have_rows('clients') ): ?>
